6df83603748a32a59ae3e2e97b2690eebef93641
h0jicha <mail@hojicha.dev>
2022-11-10 08:31:41 +0900

    generate phpDocs of the models by laravel-ide-helper

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Report
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report query()
+ * @mixin \Eloquent
+ */
 class Report extends Model
 {
     use HasFactory;
