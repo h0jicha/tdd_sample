940df2d020707e672baf2d37f04a51d7f74b8a74
h0jicha <mail@hojicha.dev>
2022-11-10 08:34:14 +0900

    generate phpDocs

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,6 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Customer
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Report[] $reports
+ * @property-read int|null $reports_count
+ * @method static \Database\Factories\CustomerFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
     use HasFactory;
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -4,6 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+/**
+ * App\Models\Report
+ *
+ * @property-read \App\Models\Customer|null $customer
+ * @method static \Database\Factories\ReportFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report query()
+ * @mixin \Eloquent
+ */
 class Report extends Model
 {
     use HasFactory;
